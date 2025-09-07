c707794507cc8229a04e0d7cd69a5959e05d4dbb
putraadityapriyono37 <putraadityap1212@gmail.com>
2025-09-07 21:12:51 +0700

    project fix bismillah

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -1,13 +1,6 @@
-// src/types/menu.ts
-
-export type SubMenuItem = {
-  label: string;
-  href: string;
-};
-
 export type MenuItem = {
   id: number;
   label: string;
   href?: string;
-  submenu?: MenuItem[];
+  submenu?: MenuItem[]; // support nested menu
 };
