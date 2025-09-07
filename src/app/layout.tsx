f31a83fa6c51bfbb4a7e9710a1cacf1223fe178e
Azka yann <azkayann@gmail.com>
2025-09-07 20:00:57 +0700

    hapus font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
-// src/app/layout.tsx
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const cutoffPro = localFont({
-  src: "../fonts/Cutoff_Pro.otf",  // relatif dari src/app/layout.tsx
-  display: "swap",
-  variable: "--font-cutoff-pro",
-});
 export const metadata: Metadata = {
   title: "BatikLens",
   description: "Kenali dan Ciptakan Karya Batik Pilihanmu",
@@ -15,12 +8,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={`${cutoffPro.variable} font-cutoff antialiased`}>
+      <body className="font-sans antialiased">
         {children}
       </body>
     </html>
